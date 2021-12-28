0cab922cd6906e138b4cab4b444176cfcba8d944
Daniel Wagner <wagner.daniel87@gmail.com>
2021-12-28 15:23:44 +0100

    added meetup detail page and some html optimizations

--- meetups/views.py
+++ meetups/views.py
@@ -6,8 +6,15 @@ def index(request):
         {'title': 'A first Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
         {'title': 'A Second Meetup', 'location': 'Paris', 'slug': 'a-second-meetup'}
     ]
-    
+
     return render(request, template_name='meetups/index.html', context={
         'show_meetups': len(meetups) > 0,
         'meetups': meetups
+    })
+
+
+def meetup_details(request, meetup_slug):
+    selected_meetup = {'title': 'A first Meetup', 'location': 'New York', 'description': 'The description of the first meetup'},
+    return render(request, template_name='meetups/details.html', context={
+        'meetup': selected_meetup
     })
