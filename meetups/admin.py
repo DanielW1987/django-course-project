fdba9214f0f0c12dba54b323c5e5167c384745e7
Daniel Wagner <wagner.daniel87@gmail.com>
2022-01-07 17:06:48 +0100

    add Location as new model

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from meetups.models import Meetup
+from meetups.models import Meetup, Location
 
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'slug')
-    list_filter = ('title',)
+    list_filter = ('title', 'location',)
     prepopulated_fields = {'slug': ('title', )}
 
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
--- meetups/models.py
+++ meetups/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 
+class Location(models.Model):
+    name: str = models.CharField(max_length=200)
+    address: str = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+
 class Meetup(models.Model):
     title: str = models.CharField(max_length=100)
-    # location: str = models.CharField(max_length=100)
     description: str = models.TextField()
     slug: str = models.SlugField(unique=True)
     image = models.ImageField(upload_to='images', default='')
+    location: Location = models.ForeignKey(to=Location, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.pk} - {self.title} '
-
