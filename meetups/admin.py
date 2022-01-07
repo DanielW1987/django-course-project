57d68454ba490415e775b122a3135d41b772edbf
Daniel Wagner <wagner.daniel87@gmail.com>
2022-01-07 17:32:24 +0100

    add Participant as new model

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from meetups.models import Meetup, Location
+from meetups.models import Meetup, Location, Participant
 
 
 class MeetupAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/models.py
+++ meetups/models.py
@@ -1,20 +1,32 @@
+from typing import List
+
 from django.db import models
 
 
 class Location(models.Model):
     name: str = models.CharField(max_length=200)
-    address: str = models.CharField(max_length=300)
+    address: str = models.CharField(max_length=300, blank=True, null=True)
 
     def __str__(self):
         return f'{self.name} ({self.address})'
 
 
+class Participant(models.Model):
+    firstName: str = models.CharField(max_length=50)
+    lastName: str = models.CharField(max_length=50, blank=True, null=True)
+    email: str = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f'{self.firstName} {self.lastName}'
+
+
 class Meetup(models.Model):
     title: str = models.CharField(max_length=100)
-    description: str = models.TextField()
+    description: str = models.TextField(blank=True, null=True)
     slug: str = models.SlugField(unique=True)
-    image = models.ImageField(upload_to='images', default='')
-    location: Location = models.ForeignKey(to=Location, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', default='', blank=True, null=True)
+    location: Location = models.ForeignKey(to=Location, on_delete=models.CASCADE, blank=True, null=True)
+    participants: List = models.ManyToManyField(to=Participant, blank=True)
 
     def __str__(self):
         return f'{self.pk} - {self.title} '
